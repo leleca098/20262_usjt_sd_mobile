a0d4c24ef0c955e9cd909dbcfa50d9c0abf2afc0
Leticia Augusto da Silva <8261108611@ulife.com.br>
2026-08-20 21:14:47 -0300

    learn(ap01 -js) encerra codigo 1.1.2

---
+++ 02_intro_js/script.js
@@ -0,0 +1,21 @@
+//declaração de variáveis e constantes
+//const, let e var
+//const nome = 'José'
+//nomp = "José Silva"
+//let idade = 17
+//idade = 18
+//var sobrenome = "Soares"
+//sobrenome = "Silva"
+
+
+// var linguagem = "Java"
+// console.log("Aprendendo " + linguagem)
+// var linguagem = "Javascript"
+// console.log(`Aprendendo ${linguagem}`)
+let idade = 18
+console.log(`Oi, ${nome}`)
+if(idade >=18) {
+    let nome = "José"
+    console.log(`Parabéns ${nome}. Você pode dirigir`)
+}
+console.log(`Até, ${nome}`)
