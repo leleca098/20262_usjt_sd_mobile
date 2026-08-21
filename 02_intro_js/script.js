f3c01eefb182bcdf22b73703c8ad54bad56e2153
Leticia Augusto da Silva <8261108611@ulife.com.br>
2026-08-20 21:45:59 -0300

    learn(ap01-js): encerra codigo 1.3.1

--- 02_intro_js/script.js
+++ 02_intro_js/script.js
@@ -12,10 +12,22 @@
 // console.log("Aprendendo " + linguagem)
 // var linguagem = "Javascript"
 // console.log(`Aprendendo ${linguagem}`)
-let idade = 18
-console.log(`Oi, ${nome}`)
-if(idade >=18) {
-    let nome = "José"
-    console.log(`Parabéns ${nome}. Você pode dirigir`)
-}
-console.log(`Até, ${nome}`)
+// let idade = 18
+// console.log(`Oi, ${nome}`)
+// if(idade >=18) {
+//     let nome = "José"
+//     console.log(`Parabéns ${nome}. Você pode dirigir`)
+// }
+// console.log(`Até, ${nome}`)
+
+//coerçao
+//implicita e explicita
+const n1 = 2
+const n2 = '3'
+//coerçao implicita
+const n3 = n1 + n2
+console.log(n3)
+//coerçao explicita
+const n4 = n1 + Number(n2)
+console.log(n4)
+
